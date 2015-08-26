4c72df5b85e9ad1ad0d50a4040543c6100b5c32b
Abdella <abdellaalioncan@gmail.com>
2015-08-25 23:19:49 -0400

    hacky but working, nearly done prototype combination

--- components/home/home.js
+++ components/home/home.js
@@ -5,11 +5,42 @@ angular.module('pokemon-search.home', ['pokemon-search.xy-search'])
 
     (function(vm){
 
-      xyPokemon.getMappedPokemon().then(function(x){
-        var byPokemon = R.groupBy(function(encounterByPokemon){
-           return encounterByPokemon.pokemon_id;
-        }, x);
-        vm.nearlyRaw = byPokemon;
+
+      //Callback hell, will do better eventually, this is just quick
+      //Also, these variable names are the worst
+      xyPokemon.getMappedPokemon().then(function(encounterData){
+
+        xyPokemon.getLocationAreaInfo()
+          .then(function(locationAreaInfo){
+
+            //No reason locationIDs shouldn't be on every encounter
+            var encountersWithLocation = R.map(function(encounterInfo){
+              encounterInfo.locationID = R.find(function(areaInfo){
+                return areaInfo.id === encounterInfo.location_area_id;
+              }, locationAreaInfo.data).location_id;
+              return encounterInfo;
+            }, encounterData);
+
+            xyPokemon.getLocationNames()
+              .then(function(locationNames){
+
+                encountersWithLocation = R.map(function(encounter){
+                  encounter.locationName = R.find(function(locationInfo){
+                    return locationInfo.location_id === encounter.locationID;
+                  }, locationNames).name;
+                  return encounter;
+                }, encountersWithLocation);
+
+
+
+                vm.nearlyRaw = R.groupBy(function(encounterByPokemon){
+                  return encounterByPokemon.pokemon_id;
+                }, encountersWithLocation);
+
+              });
+          });
+
+
       });
 
     })(this);
--- scripts/xy-pokemon-by-location-service.js
+++ scripts/xy-pokemon-by-location-service.js
@@ -10,7 +10,24 @@ angular.module('pokemon-search.xy-search', [])
       });
     }
 
+    function getLocationNames() {
+      return $http.get('raw_data/locations.json').then(function(allResults){
+        return R.filter(R.propEq('local_language_id', 9), allResults.data);
+      });
+    }
+
+    function getLocationAreaInfo() {
+      return $http.get('raw_data/location_areas.json');
+    }
+
+    function getAllPokemonInfo(){
+      return $http.get('raw_data/pokemon.json');
+    }
+
     return {
-      getMappedPokemon: getMappedPokemon
+      getMappedPokemon: getMappedPokemon,
+      getLocationNames: getLocationNames,
+      getLocationAreaInfo: getLocationAreaInfo,
+      getAllPokemonInfo: getAllPokemonInfo
     }
   }]);
