eb1c8e6761a553f2ba365d5bc3840f38e6595199
Abdella <abdellaalioncan@gmail.com>
2015-08-25 23:42:23 -0400

    all pokemon and their encounters

--- components/home/home.js
+++ components/home/home.js
@@ -3,45 +3,52 @@
 angular.module('pokemon-search.home', ['pokemon-search.xy-search'])
   .controller('HomeController', ['xyPokemon', function (xyPokemon) {
 
-    (function(vm){
+    (function (vm) {
 
 
       //Callback hell, will do better eventually, this is just quick
       //Also, these variable names are the worst
-      xyPokemon.getMappedPokemon().then(function(encounterData){
+      xyPokemon.getMappedPokemon().then(function (encounterData) {
 
         xyPokemon.getLocationAreaInfo()
-          .then(function(locationAreaInfo){
+          .then(function (locationAreaInfo) {
 
             //No reason locationIDs shouldn't be on every encounter
-            var encountersWithLocation = R.map(function(encounterInfo){
-              encounterInfo.locationID = R.find(function(areaInfo){
+            var encountersWithLocation = R.map(function (encounterInfo) {
+              encounterInfo.locationID = R.find(function (areaInfo) {
                 return areaInfo.id === encounterInfo.location_area_id;
               }, locationAreaInfo.data).location_id;
               return encounterInfo;
             }, encounterData);
 
             xyPokemon.getLocationNames()
-              .then(function(locationNames){
+              .then(function (locationNames) {
 
-                encountersWithLocation = R.map(function(encounter){
-                  encounter.locationName = R.find(function(locationInfo){
+                encountersWithLocation = R.map(function (encounter) {
+                  encounter.locationName = R.find(function (locationInfo) {
                     return locationInfo.location_id === encounter.locationID;
                   }, locationNames).name;
                   return encounter;
                 }, encountersWithLocation);
 
-
-
-                vm.nearlyRaw = R.groupBy(function(encounterByPokemon){
+                encountersWithLocation = R.groupBy(function (encounterByPokemon) {
                   return encounterByPokemon.pokemon_id;
                 }, encountersWithLocation);
 
+
+                xyPokemon.getAllPokemonInfo()
+                  .then(function (pokemons) {
+
+                    vm.allPokemon = R.map(function (pokemon) {
+                      pokemon.encounters = encountersWithLocation[pokemon.id];
+                      return pokemon;
+                    }, pokemons.data);
+                  });
               });
+
           });
 
-
-      });
-
+      })
     })(this);
+
   }]);
